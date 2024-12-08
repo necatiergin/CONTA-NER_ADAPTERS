80f07c6cd80a27ef5556d2d9b7feb6731974b819
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-08 15:38:28 +0300

    Create stack_03.cpp

---
+++ stack_03.cpp
@@ -0,0 +1,12 @@
+#include <stack>
+#include <vector>
+#include <list>
+
+
+int main()
+{
+	std::stack<int> st1;
+	std::stack<int, std::vector<int>> st2;
+	std::stack<int, std::list<int>> st3;
+	//...
+}
